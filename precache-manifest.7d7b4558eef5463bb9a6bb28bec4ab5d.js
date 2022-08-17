eb836a986dd5ed675a8b631d92d65eef3f118593
fadhlur <fadhlurthe.silent@gmail.com>
2022-08-17 14:19:01 +0700

    Updates

---
+++ precache-manifest.7d7b4558eef5463bb9a6bb28bec4ab5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "939000fb2efa4e506513",
+    "url": "/Onnote-app/static/css/main.54a42e5d.chunk.css"
+  },
+  {
+    "revision": "939000fb2efa4e506513",
+    "url": "/Onnote-app/static/js/main.939000fb.chunk.js"
+  },
+  {
+    "revision": "f96e91e00a484af57ce9",
+    "url": "/Onnote-app/static/js/1.f96e91e0.chunk.js"
+  },
+  {
+    "revision": "37e999db2d3da191902c",
+    "url": "/Onnote-app/static/js/runtime~main.37e999db.js"
+  },
+  {
+    "revision": "7a18e25a8b335bfc91e8638e206b94f4",
+    "url": "/Onnote-app/index.html"
+  }
+];
